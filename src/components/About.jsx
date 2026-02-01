11009099646ce986870d63d05da6c04e27cd5760
Abuzar Khan <105778612+abuzarkhan1@users.noreply.github.com>
2026-02-01 19:55:05 +0500

    Update APK download link to version 2.3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
 
     const link = document.createElement("a");
     link.href =
-      "https://github.com/abuzarkhan1/ArenaX-Web/releases/download/v2.2/ArenaX.apk";
+     "https://github.com/abuzarkhan1/ArenaX-Web/releases/download/v2.3/ArenaX.apk";
     link.download = "ArenaX.apk";
     link.target = "_blank";
     document.body.appendChild(link);
